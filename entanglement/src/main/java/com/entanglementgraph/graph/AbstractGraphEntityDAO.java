6f3a0ebac6fcc887e241ea34a08813c9b9254265
Keith Flanagan <k.s.flanagan@gmail.com>
2013-03-22 16:15:26 +0000

    Fixed a NullPointerException. Not sure if this is a Mongo bug? Apparently, we can't use the 'dot notation' when obtaining a nested property from an item returned by a DBCursor. In this case, we were getting 'null' when attempting to obtain 'keys.type'. However, as you can see getting the BasicDBObject 'keys', followed by 'type' works.

--- entanglement/src/main/java/com/entanglementgraph/graph/AbstractGraphEntityDAO.java
+++ entanglement/src/main/java/com/entanglementgraph/graph/AbstractGraphEntityDAO.java
@@ -417,7 +417,18 @@ abstract public class AbstractGraphEntityDAO
         return false;
       }
       for (DBObject next : result) {
-        String nextType = (String) next.get(FIELD_KEYS_TYPE);
+        //FIXME at some point, find out why this doesn't work... (FIELD_KEYS_TYPE == "keys.type")
+//        String nextType = (String) next.get(FIELD_KEYS_TYPE);
+        //FIXME ... and why this does.
+        String nextType = (String) ((BasicDBObject)next.get("keys")).get("type");
+
+        if (nextType == null) {
+          throw new GraphModelException("When performing a query with one or more entity names, one or more of the " +
+              "result items had a entity type of NULL. Your dataset therefore inconsistent, since an entity type is " +
+              "required when specifying one or more entity names.\n" +
+              "Query was: "+query +
+              "\nOffending item was: "+next);
+        }
         if (nextType.equals(entityType)) {
           return true;
         }
