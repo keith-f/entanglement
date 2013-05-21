d185ff0b54d426b569dafd9b12f1470a5badbb7b
Keith Flanagan <k.s.flanagan@gmail.com>
2013-05-21 14:50:03 +0100

    Javadoc and methods renamed for clarity

--- entanglement/src/main/java/com/entanglementgraph/util/TxnUtils.java
+++ entanglement/src/main/java/com/entanglementgraph/util/TxnUtils.java
@@ -38,21 +38,29 @@ public class TxnUtils
 {
   private static final Logger logger = Logger.getLogger(TxnUtils.class.getName());
 
-  public static void writeOperationAsPatch(GraphConnection conn, GraphOperation graphOp) throws RevisionLogException {
+  /**
+   * Convenience method that takes a single graph operation, wraps it in a transaction and submits it to the
+   * revision log. This method is useful if you have an individual graph operation to submit as part
+   * of a single patch set and don't want to deal with creating / committing transactions manually.
+   * @param conn
+   * @param graphOp the graph operation to apply.
+   * @throws RevisionLogException
+   */
+  public static void submitAsTxn(GraphConnection conn, GraphOperation graphOp) throws RevisionLogException {
     List<GraphOperation> ops = new ArrayList<>(1);
     ops.add(graphOp);
-    writeOperationsAsPatch(conn, ops);
+    submitAsTxn(conn, ops);
   }
 
   /**
-   * Convenience method that takes a list of graph operations, wraps them in a transaction and submits it to the
+   * Convenience method that takes a list of graph operations, wraps them in a transaction and submits them to the
    * revision log. This method is useful if you have a relatively small number of graph operations to submit as part
    * of a single transaction; you don't need to deal with creating / committing transactions manually in this case.
    * @param conn
-   * @param ops
+   * @param ops the graph operations to apply.
    * @throws RevisionLogException
    */
-  public static void writeOperationsAsPatch(GraphConnection conn, List<GraphOperation> ops) throws RevisionLogException {
+  public static void submitAsTxn(GraphConnection conn, List<GraphOperation> ops) throws RevisionLogException {
     String txnId = null;
     if (ops.isEmpty()) {
       return;
@@ -60,7 +68,6 @@ public class TxnUtils
     try {
       txnId = TxnUtils.beginNewTransaction(conn);
       conn.getRevisionLog().submitRevisions(conn.getGraphName(), conn.getGraphBranch(), txnId, 1, ops);
-      ops.clear();
       TxnUtils.commitTransaction(conn, txnId);
     } catch (Exception e) {
       TxnUtils.silentRollbackTransaction(conn, txnId);
@@ -68,6 +75,46 @@ public class TxnUtils
           +conn.getGraphBranch()+"/"+conn.getGraphBranch()
           +". Exception attached.", e);
     }
+  }
+
+
+  /**
+   * Submits a batch of operations as part of a transaction. You must specify a transaction ID, and a <code>partId</code>.
+   * This main use-cases of this method are as follows:
+   * <ul>
+   *   <li>You need to submit a massive number of graph operations as part of the same transaction (too many to submit
+   *   with the <code>submitAsTxn</code> method.</li>
+   *   <li>You have a cluster of machines whose combined output should be considered as part of the same transaction.</li>
+   * </ul>
+   *
+   * In both of these cases, you need to manage the start, submission and end of a transaction block manually. This is
+   * achieved by:
+   * <ol>
+   *   <li>A call to <code>beginNewTransaction</code> in order to obtain a new String transaction ID</li>
+   *   <li>One or more calls to <code>submitTxnPart</code> (this method) to submit graph operations</li>
+   *   <li>A final call to either <code>commitTransaction</code> or <code>rollbackTransaction</code> to mark the
+   *   end of a transaction block.</li>
+   * </ol>
+   *
+   * When calling <code>submitTxnPart</code>, you need to specify an integer <code>partId</code>. This number should
+   * be zero or positive, and determines the order that elements submitted in this transaction part are processed in,
+   * with respect to other pars. The value of <code>partId</code> only really matters when order is important - for
+   * example, if you want to ensure that a deletion occurs before an update.
+   *
+   * Transaction parts can be submitted out of order, but are played back in ascending order. This allows, multiple
+   * machines in a cluster to complete work at their own rate, but be able to coordinate an order-dependent set of
+   * graph updates as a single graph 'transaction'.
+   * 
+   * @param conn the connection information to use - a convenient way to obtain the graph name/branch information.
+   * @param txnId the unique ID of this transaction
+   * @param partId an integer ID for this submission that will determine the order in which it is played back on
+   *               commit.
+   * @param ops the graph operations that form this part of the transaction.
+   * @throws RevisionLogException
+   */
+  public static void submitTxnPart(GraphConnection conn, String txnId, int partId, List<GraphOperation> ops) 
+      throws RevisionLogException {
+    conn.getRevisionLog().submitRevisions(conn.getGraphName(), conn.getGraphBranch(), txnId, partId, ops);
   }
 
   public static String beginNewTransaction(GraphConnection conn)
