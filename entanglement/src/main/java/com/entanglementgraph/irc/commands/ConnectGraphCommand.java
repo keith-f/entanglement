e7430162d19ef6862a03963a2710370d16d702a6
Keith Flanagan <k.s.flanagan@gmail.com>
2013-06-10 19:41:21 +0100

    Fixed typo

--- entanglement/src/main/java/com/entanglementgraph/irc/commands/ConnectGraphCommand.java
+++ entanglement/src/main/java/com/entanglementgraph/irc/commands/ConnectGraphCommand.java
@@ -74,7 +74,7 @@ public class ConnectGraphCommand extends AbstractCommand<EntanglementRuntime> {
     String hostname = parsedArgs.get("hostname").getStringValue();
     String database = parsedArgs.get("database").getStringValue();
     String graph = parsedArgs.get("graph").getStringValue();
-    String branch = parsedArgs.get("trunk").getStringValue();
+    String branch = parsedArgs.get("branch").getStringValue();
 
     try {
       GraphConnection connection = connect(hostname, database, graph, branch);
