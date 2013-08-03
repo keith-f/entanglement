8d8e375e8498a1f691880c54158256c8894001d0
Keith Flanagan <k.s.flanagan@gmail.com>
2013-08-03 21:36:40 +0100

    Added a rule that stops graph iteration once a specified depth (steps away from the start node) has been reached

---
+++ entanglement/src/main/java/com/entanglementgraph/iteration/StopAfterDepthRule.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2013 Keith Flanagan
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.entanglementgraph.iteration;
+
+import com.entanglementgraph.cursor.GraphCursor;
+import com.entanglementgraph.graph.data.Edge;
+import com.entanglementgraph.graph.data.EntityKeys;
+import com.entanglementgraph.graph.data.Node;
+import com.entanglementgraph.revlog.commands.EdgeModification;
+import com.entanglementgraph.revlog.commands.MergePolicy;
+import com.entanglementgraph.revlog.commands.NodeModification;
+import com.mongodb.BasicDBObject;
+
+/**
+ * A rule that causes graph iterations to stop when a particular depth (steps away from the starting node)
+ * is reached.
+ *
+ * @author Keith Flanagan
+ */
+public class StopAfterDepthRule extends AbstractRule {
+  private final int targetDepth;
+
+  public StopAfterDepthRule(int targetDepth) {
+    this.targetDepth = targetDepth;
+  }
+
+  @Override
+  public boolean ruleMatches(String cursorName, int currentDepth, EntityKeys<? extends Node> currentPosition,
+                             GraphCursor.NodeEdgeNodeTuple nenTuple,
+                             boolean outgoingEdge, EntityKeys<Node> nodeId, EntityKeys<Edge> edgeId) throws RuleException  {
+    return currentDepth > targetDepth;
+  }
+
+  @Override
+  public HandlerAction apply(String cursorName, int currentDepth, EntityKeys<? extends Node> currentPosition,
+                             GraphCursor.NodeEdgeNodeTuple nenTuple,
+                             boolean outgoingEdge, EntityKeys<Node> nodeId, EntityKeys<Edge> edgeId) throws RuleException {
+    HandlerAction action = new HandlerAction(NextEdgeIteration.TERMINATE_BRANCH);
+    return action;
+  }
+}
