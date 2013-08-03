5079867f645cbe98d49e72cbe54b2eb31d33cf42
Keith Flanagan <k.s.flanagan@gmail.com>
2013-08-03 21:08:45 +0100

    Added a rule that can be used as a filter when starting graph iterations. For example, if you don't want an iteration to occur unless the start node is of a specified type

---
+++ entanglement/src/main/java/com/entanglementgraph/iteration/StartNodeTypeFilterRule.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2013 Keith Flanagan
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+package com.entanglementgraph.iteration;
+
+import com.entanglementgraph.cursor.GraphCursor;
+import com.entanglementgraph.graph.data.Edge;
+import com.entanglementgraph.graph.data.EntityKeys;
+import com.entanglementgraph.graph.data.Node;
+import com.entanglementgraph.revlog.commands.GraphOperation;
+import com.entanglementgraph.revlog.commands.MergePolicy;
+import com.entanglementgraph.revlog.commands.NodeModification;
+import com.mongodb.BasicDBObject;
+
+import java.util.List;
+
+/**
+ * A rule that can be used in cases where a subgraph should only be built if the cursor start position is located
+ * on a node of a particular type. If the initial cursor node is of the specified type, then it is added.
+ * If the initial cursor node is not of the specified type, then this rule causes the termination of the iteration.
+ *
+ * @author Keith Flanagan
+ */
+public class StartNodeTypeFilterRule extends AbstractRule {
+  private final String startTypeName;
+
+  private boolean rootNodeWasValidStartType;
+
+  public StartNodeTypeFilterRule(String startTypeName) {
+    this.startTypeName = startTypeName;
+  }
+
+  @Override
+  public List<GraphOperation> iterationStarted(String cursorName, EntityKeys<? extends Node> currentPosition) throws RuleException {
+    rootNodeWasValidStartType = currentPosition.getType().equals(startTypeName);
+    List<GraphOperation> ops = super.iterationStarted(cursorName, currentPosition);
+    if (!rootNodeWasValidStartType) {
+      return ops;
+    }
+
+    try {
+      BasicDBObject currentNode = sourceGraph.getNodeDao().getByKey(currentPosition);
+      ops.add(new NodeModification(MergePolicy.APPEND_NEW__LEAVE_EXISTING, currentNode));
+    } catch (Exception e) {
+      throw new RuleException("Failed to retrieve start node content", e);
+    }
+
+    return ops;
+  }
+
+  @Override
+  public boolean ruleMatches(String cursorName, EntityKeys<? extends Node> currentPosition, GraphCursor.NodeEdgeNodeTuple nenTuple, boolean outgoingEdge, EntityKeys<Node> nodeId, EntityKeys<Edge> edgeId) throws RuleException {
+    return !rootNodeWasValidStartType;
+  }
+
+  @Override
+  public HandlerAction apply(String cursorName, EntityKeys<? extends Node> currentPosition, GraphCursor.NodeEdgeNodeTuple nenTuple, boolean outgoingEdge, EntityKeys<Node> nodeId, EntityKeys<Edge> edgeId) throws RuleException {
+    return new HandlerAction(NextEdgeIteration.TERMINATE);
+  }
+}
