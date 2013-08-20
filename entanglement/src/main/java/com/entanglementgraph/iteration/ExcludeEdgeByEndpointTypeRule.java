3d904bcb99c2985f980b74381e7759045eb21431
Keith Flanagan <k.s.flanagan@gmail.com>
2013-08-20 17:58:56 +0100

    Added a number if new iteration rules

---
+++ entanglement/src/main/java/com/entanglementgraph/iteration/ExcludeEdgeByEndpointTypeRule.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2013 Keith Flanagan
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.entanglementgraph.iteration;
+
+import com.entanglementgraph.graph.data.Edge;
+import com.entanglementgraph.graph.data.EntityKeys;
+import com.entanglementgraph.graph.data.Node;
+import com.entanglementgraph.revlog.commands.EdgeModification;
+import com.entanglementgraph.revlog.commands.MergePolicy;
+import com.entanglementgraph.revlog.commands.NodeModification;
+import com.mongodb.BasicDBObject;
+
+/**
+ * A rule that causes graph iterations to stop when an edge is encountered that starts and ends at nodes of
+ * specified types.
+ * This rule allows you to exclude specific edges.
+ *
+ * User: keith
+ * Date: 25/07/13; 16:06
+ *
+ * @author Keith Flanagan
+ */
+public class ExcludeEdgeByEndpointTypeRule extends AbstractRule {
+  private String currentNodeTargetType;
+  private String remoteNodeTargetType;
+
+  public ExcludeEdgeByEndpointTypeRule(String currentNodeTargetType, String remoteNodeTargetType) {
+    this.currentNodeTargetType = currentNodeTargetType;
+    this.remoteNodeTargetType = remoteNodeTargetType;
+  }
+
+  @Override
+  public boolean ruleMatches(String cursorName, int currentDepth,
+                             EntityKeys<? extends Node> currentPosition,
+                             EntityKeys<? extends Edge> edgeId, boolean outgoingEdge,
+                             EntityKeys<? extends Node> remoteNodeId,
+                             BasicDBObject rawLocalNode, BasicDBObject rawEdge, BasicDBObject rawRemoteNode) throws RuleException  {
+    return currentPosition.getType().equals(currentNodeTargetType)
+        && remoteNodeId.getType().equals(remoteNodeTargetType);
+  }
+
+  @Override
+  public HandlerAction apply(String cursorName, int currentDepth,
+                             EntityKeys<? extends Node> currentPosition,
+                             EntityKeys<? extends Edge> edgeId, boolean outgoingEdge,
+                             EntityKeys<? extends Node> remoteNodeId,
+                             BasicDBObject rawLocalNode, BasicDBObject rawEdge, BasicDBObject rawRemoteNode) throws RuleException {
+    HandlerAction action = new HandlerAction(NextEdgeIteration.TERMINATE_BRANCH);
+
+//    if (includeNodeInDestination) {
+//      action.getOperations().add(new NodeModification(MergePolicy.APPEND_NEW__LEAVE_EXISTING, rawRemoteNode));
+//      action.getOperations().add(new EdgeModification(MergePolicy.APPEND_NEW__LEAVE_EXISTING, rawEdge));
+//    }
+
+    return action;
+  }
+}
---
+++ entanglement/src/main/java/com/entanglementgraph/iteration/ExcludeEdgeByFromToEndpointTypeRule.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2013 Keith Flanagan
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.entanglementgraph.iteration;
+
+import com.entanglementgraph.graph.data.Edge;
+import com.entanglementgraph.graph.data.EntityKeys;
+import com.entanglementgraph.graph.data.Node;
+import com.mongodb.BasicDBObject;
+
+/**
+ * A rule that causes graph iterations to stop when an edge is encountered that starts and ends at nodes of
+ * specified types.
+ * This rule allows you to exclude specific edges.
+ *
+ * User: keith
+ * Date: 25/07/13; 16:06
+ *
+ * @author Keith Flanagan
+ */
+public class ExcludeEdgeByFromToEndpointTypeRule extends AbstractRule {
+  private String fromNodeTargetType;
+  private String toNodeTargetType;
+
+  public ExcludeEdgeByFromToEndpointTypeRule(String fromNodeTargetType, String toNodeTargetType) {
+    this.fromNodeTargetType = fromNodeTargetType;
+    this.toNodeTargetType = toNodeTargetType;
+  }
+
+  @Override
+  public boolean ruleMatches(String cursorName, int currentDepth,
+                             EntityKeys<? extends Node> currentPosition,
+                             EntityKeys<? extends Edge> edgeId, boolean outgoingEdge,
+                             EntityKeys<? extends Node> remoteNodeId,
+                             BasicDBObject rawLocalNode, BasicDBObject rawEdge, BasicDBObject rawRemoteNode) throws RuleException  {
+    try {
+      Edge edge = sourceGraph.getMarshaller().deserialize(rawEdge, Edge.class);
+      return edge.getFrom().getType().equals(fromNodeTargetType)
+          && edge.getTo().getType().equals(toNodeTargetType);
+    } catch (Exception e) {
+      throw new RuleException("Failed to process rule", e);
+    }
+  }
+
+  @Override
+  public HandlerAction apply(String cursorName, int currentDepth,
+                             EntityKeys<? extends Node> currentPosition,
+                             EntityKeys<? extends Edge> edgeId, boolean outgoingEdge,
+                             EntityKeys<? extends Node> remoteNodeId,
+                             BasicDBObject rawLocalNode, BasicDBObject rawEdge, BasicDBObject rawRemoteNode) throws RuleException {
+    HandlerAction action = new HandlerAction(NextEdgeIteration.TERMINATE_BRANCH);
+
+//    if (includeNodeInDestination) {
+//      action.getOperations().add(new NodeModification(MergePolicy.APPEND_NEW__LEAVE_EXISTING, rawRemoteNode));
+//      action.getOperations().add(new EdgeModification(MergePolicy.APPEND_NEW__LEAVE_EXISTING, rawEdge));
+//    }
+
+    return action;
+  }
+}
